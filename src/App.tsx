8074c6744e4d0b254d805719c02d42353323ea81
FujiiWill <fujiiwiliam@gmail.com>
2024-02-01 11:00:10 -0300

    PrivateRoute

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import { ContextCartCount } from "./utils/context-cart";
 import Login from "./routs/ClientHome/Login";
 import Admin from "./routs/Admin";
 import AdminHome from "./routs/Admin/AdminHome";
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
-import {history} from './utils/history';
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { history } from "./utils/history";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -30,7 +31,7 @@ export default function App() {
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
           </Route>
-          <Route path="/admin/" element={<Admin />}>
+          <Route path="/admin/" element={<PrivateRoute><Admin /></PrivateRoute>}>
             <Route index element={<AdminHome />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
