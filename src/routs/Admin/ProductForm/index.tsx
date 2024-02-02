99c4a2d3275bdee77d028dcaea68593ac3564f41
FujiiWill <fujiiwiliam@gmail.com>
2024-02-02 18:33:40 -0300

    Error CSS

--- src/routs/Admin/ProductForm/index.tsx
+++ src/routs/Admin/ProductForm/index.tsx
@@ -8,13 +8,12 @@ import * as forms from "../../../utils/forms";
 import * as productService from "../../../services/product-service";
 
 export default function ProductForm() {
-
   const params = useParams();
 
-  const isEditing = params.productId !== 'create';
-  
+  const isEditing = params.productId !== "create";
+
   const [formData, setFormData] = useState<any>({
-      name: {
+    name: {
       value: "",
       id: "name",
       name: "name",
@@ -22,15 +21,15 @@ export default function ProductForm() {
       placeholder: "Nome",
     },
     price: {
-      value: "200",
+      value: "",
       id: "price",
       name: "price",
       type: "number",
       placeholder: "Preço",
-      validation: function(value: any){
+      validation: function (value: any) {
         return Number(value) > 0;
       },
-      message: "Favor informar um valor positivo"
+      message: "Favor informar um valor positivo",
     },
     imgUrl: {
       value: "",
@@ -41,22 +40,23 @@ export default function ProductForm() {
     },
   });
 
-  useEffect (() =>{
-
-    const obj = forms.validate(formData, "price");
-    console.log(obj);
-
-    if(isEditing){
-      productService.findById(Number(params.productId))
-        .then(response => {
-          const newFormData = forms.updateAll(formData, response.data);
-          setFormData(newFormData);
-        });
+  useEffect(() => {
+    if (isEditing) {
+      productService.findById(Number(params.productId)).then((response) => {
+        const newFormData = forms.updateAll(formData, response.data);
+        setFormData(newFormData);
+      });
     }
   }, []);
 
   function handleInputChange(event: any) {
-    setFormData(forms.update(formData, event.target.name, event.target.value));
+    const dataUpdated = forms.update(
+      formData,
+      event.target.name,
+      event.target.value
+    );
+    const dataValidated = forms.validate(dataUpdated, event.target.name);
+    setFormData(dataValidated);
   }
 
   return (
@@ -72,6 +72,7 @@ export default function ProductForm() {
                   className="dsc-form-control"
                   onChange={handleInputChange}
                 />
+                <div className="dsc-form-error">{formData.name.message}</div>
               </div>
               <div>
                 <FormInput
@@ -79,6 +80,7 @@ export default function ProductForm() {
                   className="dsc-form-control"
                   onChange={handleInputChange}
                 />
+                <div className="dsc-form-error">{formData.price.message}</div>
               </div>
               <div>
                 <FormInput
