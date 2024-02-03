c7168fb39beb2402c137000583e9b838648d1a31
FujiiWill <fujiiwiliam@gmail.com>
2024-02-03 10:28:58 -0300

    forms dirty, dirty behaviour

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -4,7 +4,16 @@
 export default function FormInput(props: any) {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { validation, invalid, ...inputProps } = props;
+  const { validation, invalid = "false", dirty = "false", onTurnDirty, ...inputProps } = props;
 
-  return <input {...inputProps} data-invalid={invalid} />;
+  function handleBlur(){
+    onTurnDirty(props.name);
+  }
+
+  return <input 
+    {...inputProps}
+    onBlur={handleBlur} 
+    data-invalid={invalid} 
+    data-dirty={dirty}
+  />;
 }
