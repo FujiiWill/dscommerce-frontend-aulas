7d43d927af12700f363c7b79f04d4d2631fc0404
FujiiWill <fujiiwiliam@gmail.com>
2024-02-03 12:25:05 -0300

    FormTextArea

---
+++ src/components/FormTextArea/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+export default function FormTextArea(props: any) {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { validation, invalid = "false", dirty = "false", onTurnDirty, ...textAreaProps } = props;
+
+  function handleBlur(){
+    onTurnDirty(props.name);
+  }
+
+  return <input 
+    {...textAreaProps}
+    onBlur={handleBlur} 
+    data-invalid={invalid} 
+    data-dirty={dirty}
+  />;
+}
